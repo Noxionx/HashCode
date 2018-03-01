67fa79d20fa6aa7ba97c810cb13c5e30c316a655
jimmy <jimmy.leray@protonmail.com>
2018-03-01 21:24:43 +0100

    first format data

--- common/util.js
+++ common/util.js
@@ -1,3 +1,1 @@
-module.exports.calcul_distance = ({x1,y1,x2,y2}) => Math.abs(x1-x2) + Math.abs(y1-y2);
-
-
+module.exports.calcul_distance = ({ x1, y1, x2, y2 }) => Math.abs(x1 - x2) + Math.abs(y1 - y2)
