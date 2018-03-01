9bad2138222e238f50cf3917bdc1369ecda92e4a
Tanguy <bernardtanguy1pro@gmail.com>
2018-03-01 20:20:41 +0100

    calcul how long does it takes

--- common/util.js
+++ common/util.js
@@ -1,3 +1,22 @@
 module.exports.calcul_distance = ({x1,y1,x2,y2}) => Math.abs(x1-x2) + Math.abs(y1-y2);
 
 
+/**
+ * a position abscisse vehicule
+ * b position ord vehicule
+ * x destination abs veh
+ * y destination ord veh
+ * earlier
+ * finish
+ */
+module.exports.howLong = (a,b,x,y,earlier, finish) => {
+    
+    dist =  calcul_distance(a,b,x,y) ;
+    if(dist === earlier){return 0} //arriver pile poile retour 0 (BONUS)
+    if(dist < earlier) {return  earlier-dist;} // arriver tôt (negatif)
+    if(dist > finish) {return Number.POSITIVE_INFINITY} //infini ne va pas là
+    if(dist <= finish && dist > earlier ) {return dist} //distance pour y aller (positif)
+
+}
+
+
