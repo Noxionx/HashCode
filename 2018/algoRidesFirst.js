b1fd744268e979eae05857cf77e90f1d8aa02b3c
Antoine Jouan <antoine.jouan@zenika.com>
2018-03-01 21:32:12 +0100

    first version

--- 2018/algoRidesFirst.js
+++ 2018/algoRidesFirst.js
@@ -1,12 +1,39 @@
+const util = require('../common/util');
+
 module.exports = (infos) => {
-    var cars = [];
     for (const ride of infos.rides) {
-        let bestNote = 0;
+        let maxStep = math.min(rides.ft, ride.t);
+        let bestChoice = {}    
         for (const car of infos.cars) {
             let note = calcHeuristic(car);
+            if (maxStep > note && (!bestChoice.car || bestChoice.note > note)) {
+                bestChoice.car = car;
+                bestChoice.note = note;                                
+            }
+        }
 
+        if (bestChoice.car) {
+            bestChoice.car.rides.push(ride.id);
         }
     }
-
     return infos.cars;
 }
+
+function calcHeuristic(car, ride) {
+    let note = util.calcul_distance(car.curx, car.cury, ride.sx, ride.sy);
+    note += util.calcul_distance(ride.sx,  ride.sy, ride.fx, ride.fy);
+
+    if (note < ride.st) {
+        note = ride.st;
+    }
+
+    return note;
+}
+
+function validBestChoice(ride, car, note) {
+    car.ride.push(ride.id);
+    car.t += note;
+    car.curx = ride.fx;
+    car.cury = ride.fy;
+}
+
