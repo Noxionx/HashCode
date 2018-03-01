6223449e02b694e592a21bda993be73ff266e607
Tanguy <bernardtanguy1pro@gmail.com>
2018-03-01 20:25:08 +0100

    calcul how long does it takes pow

--- common/util.js
+++ common/util.js
@@ -13,7 +13,7 @@ module.exports.howLong = ({a,b,x,y,earlier, finish}) => {
     
     dist =  calcul_distance(a,b,x,y) ;
     if(dist === earlier){return 0} //arriver pile poile retour 0 (BONUS)
-    if(dist < earlier) {return  earlier-dist;} // arriver tôt (negatif)
+    if(dist < earlier) {return  Math.pow(earlier-dist, 2);} // arriver tôt (negatif)
     if(dist > finish) {return Number.POSITIVE_INFINITY} //infini ne va pas là
     if(dist <= finish && dist > earlier ) {return dist} //distance pour y aller (positif)
 
