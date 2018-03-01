bee22bd1ce081b2b511da3b24e9c7fae53ee9f96
Tanguy <bernardtanguy1pro@gmail.com>
2018-03-01 20:38:25 +0100

    calcul how long does it takes fix add step

--- common/util.js
+++ common/util.js
@@ -9,10 +9,10 @@ module.exports.calcul_distance = ({x1,y1,x2,y2}) => Math.abs(x1-x2) + Math.abs(y
  * earlier
  * finish
  */
-module.exports.howLong = ({a,b,x,y,stepCurrent,earlier, finish}) => {
+module.exports.howLong = ({a,b,x,y,currentStep,earlier, finish}) => {
     
     dist =  calcul_distance(a,b,x,y);
-    distRestante = dist+step
+    distRestante = dist+currentStep
     if(distRestante === earlier){return 0} //arriver pile poile retour 0 (BONUS)
     if(distRestante < earlier) {return  Math.pow(earlier-distRestante, 2);} // arriver tôt (negatif)
     if(distRestante > finish) {return Number.POSITIVE_INFINITY} //infini ne va pas là
