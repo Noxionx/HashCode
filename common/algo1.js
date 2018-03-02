4af4930af1f95ee8660fbd34918dd441e9aa0881
Tanguy <bernardtanguy1pro@gmail.com>
2018-03-02 08:35:38 +0100

    first fix algo1

--- common/algo1.js
+++ common/algo1.js
@@ -1,31 +1,34 @@
 var util = require('./util')
 
 module.exports.browseBySteps = ({ cars, rides, T }) => {
-  console.log(cars)
-  let step = 0
 
+  let step = 0
+console.log(T);
   while (step <= T) {
     // Pour chaque voiture
     cars.filter(car => car.t === step).forEach(car => {
+        console.log(car.t);
       //rides possibles
-      const availableRides = rides.filter(ride => !ride.isDone)
+      var availableRides = rides.filter(ride => !ride.isDone)
       availableRides.forEach(ride => {
         ride.score = util.howLong(car.curx, car.cury, car.t, ride.st, ride.ft)
       })
       availableRides.sort((a, b) => a.score - b.score)
 
       if (availableRides.length > 0) {
-        car.t = util.calcul_distance(car.curx, car.cury, rides[0].sx, rides[0].sx)
-        car.curx = rides[0].fx
-        car.cury = rides[0].fy
-        car.rides.push(rides[0].id)
-        availableRides.shift()
+        car.t += util.calcul_distance(availableRides[0].sx, availableRides[0].sy, availableRides[0].fx, availableRides[0].fy);
+        car.curx = availableRides[0].fx
+        car.cury = availableRides[0].fy
+        car.rides.push(availableRides[0].id)
+        availableRides[0].isDone = true;
+        //console.log(car.curx);
       }
     })
+    //console.log(step)
 
     step++
   }
-  console.log(cars)
+  //console.log(cars)
 
   return cars
 }
